066825255c1e9d5019c3e56b0e18c582d748a0c0
artiq75 <artiq75@icloud.com>
2023-02-15 21:54:43 +0100

    User Authentication

---
+++ app/src/Controller/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AuthController extends AbstractController
+{
+    public function __construct(
+        private UserRepository $repository
+    )
+    {
+    }
+
+    #[Route('/connexion', name: 'login', methods: ['GET', 'POST'])]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        return $this->render('pages/auth/login.html.twig', [
+            'error' => $authenticationUtils->getLastAuthenticationError(),
+            'last_username' => $authenticationUtils->getLastUsername()
+        ]);
+    }
+
+    #[Route('/inscription', name: 'register', methods: ['GET', 'POST'])]
+    public function register(Request $request, UserPasswordHasherInterface $hasher, Security $security): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $hasher->hashPassword(
+                    $user,
+                    $user->getPassword()
+                )
+            );
+
+            $this->repository->save($user, true);
+
+            $security->login($user);
+
+            $this->addFlash('success', 'Vous ête désormais inscrit!');
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('pages/auth/register.html.twig', [
+            'form' => $form
+        ]);
+    }
+}
